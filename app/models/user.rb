f8c8956a88f09e6d6d1cfaf79af41d5152283fb5
takanashi11 <132178818+takanashi11@users.noreply.github.com>
2023-05-19 16:11:06 +0900

    リセット

--- app/models/user.rb
+++
@@ -1,9 +0,0 @@
-class User < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
-
-  validates :email, presence: true #メールアドレスが空だとエラーにさせる
-  validates :encrypted_password, presence: true #パスワードが空だとエラーにさせる
-end
