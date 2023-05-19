e0cf1848e889778913ef0b20e61dbb85f81b1176
manami0624 <manana624@yahoo.co.jp>
2023-05-19 17:16:54 +0900

    Usersモデルの作成

---
+++ db/migrate/20230519080841_devise_create_users.rb
@@ -0,0 +1,13 @@
+class CreateUsers < ActiveRecord::Migration[6.0]
+  def change
+    create_table :users do |t|
+      t.string :email ,                 null: false, unique:true
+      t.string :encrypted_password,     null: false
+      t.string :name,                   null: false
+      t.text   :profile,                null: false
+      t.text   :occupation,             null: false
+      t.text   :position,               null: false
+      t.timestamps
+    end
+  end
+end
