de412283764087eca22d287e146c73f29cc7343e
manami0624 <manana624@yahoo.co.jp>
2023-05-22 16:15:36 +0900

    新規登録の確認

--- db/migrate/20230519080841_devise_create_users.rb
+++ db/migrate/20230519080841_devise_create_users.rb
@@ -3,8 +3,8 @@
 class DeviseCreateUsers < ActiveRecord::Migration[6.0]
   def change
     create_table :users do |t|
-      t.string :email,                 null: false, unique:true
-      t.string :encrypted_password,     null: false
+      t.string :email,                 null: false, unique:true, default: ""
+      t.string :encrypted_password,     null: false, default: ""
       t.string :name,                   null: false
       t.text   :profile,                null: false
       t.text   :occupation,             null: false
--- db/schema.rb
+++ db/schema.rb
@@ -13,8 +13,8 @@
 ActiveRecord::Schema.define(version: 2023_05_19_080841) do
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.string "email", null: false
-    t.string "encrypted_password", null: false
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
     t.string "name", null: false
     t.text "profile", null: false
     t.text "occupation", null: false
