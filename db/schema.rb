8a54dfd90478392a94bf2bd702c6a31750aaa426
manami0624 <manana624@yahoo.co.jp>
2023-05-22 19:46:58 +0900

    データベースの修正

--- db/schema.rb
+++ db/schema.rb
@@ -37,8 +37,10 @@ ActiveRecord::Schema.define(version: 2023_05_19_100949) do
     t.string "title", null: false
     t.text "catch_copy", null: false
     t.text "concept", null: false
+    t.bigint "user_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_prototyps_on_user_id"
   end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
@@ -48,8 +50,13 @@ ActiveRecord::Schema.define(version: 2023_05_19_100949) do
     t.text "profile", null: false
     t.text "occupation", null: false
     t.text "position", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
