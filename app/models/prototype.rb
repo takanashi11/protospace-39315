14796b37247f9961d79e5234ff0d4c02247b62ed
taki39384 <132125615+taki39384@users.noreply.github.com>
2023-05-23 17:58:33 +0900

    コメント投稿の準備

--- app/models/prototype.rb
+++ app/models/prototype.rb
@@ -1,5 +1,6 @@
 class Prototype < ApplicationRecord
   belongs_to :user
+  has_many :comments, dependent: :destroy # commentsテーブルとのアソシエーション
   has_one_attached :image
 
   validates :image, presence: true
