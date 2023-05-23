8fea2eca461fa79e13539067d285257090385e2d
takanashi11 <132178818+takanashi11@users.noreply.github.com>
2023-05-23 12:31:36 +0900

    createアクションの記載

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -3,15 +3,23 @@ class PrototypesController < ApplicationController
   end
 
   def  new
-    @prototype = Prototype.new
+    @prototype = Prototyp.new
   end
 
   def create
+    Prototyp.create(prototype_params)
+    @prototype.save
+    if 
+     redirect_to root_path
+    else
+     render :new
+    end
 
   end
 
   private
 
   def prototype_params
-    params.require(:prototyp).permit(:image,).merge(user_id: current_user.id)
+    params.require(:prototyp).permit(:image).merge(user_id: current_user.id)
+  end
 end
