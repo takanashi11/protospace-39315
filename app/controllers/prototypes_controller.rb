e47f5ad5d68e29b038abf884a47bbcfd2d3e8fd8
manami0624 <manana624@yahoo.co.jp>
2023-05-24 16:04:39 +0900

    編集機能の実装

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -16,18 +16,35 @@ class PrototypesController < ApplicationController
     end
   end
 
+  def destroy
+  end
+
+  def edit
+    @prototype = Prototype.find(params[:id])
+  end
+
+  def update
+    @prototype = Prototype.find(params[:id])
+    if @prototype.update(prototype_params)
+    redirect_to root_path
+    else 
+      render :edit
+    end
+  end
+
   def show
     @prototype = Prototype.find(params[:id])
   end
+
   private
   def prototype_params
     params.require(:prototype).permit(:title,:catch_copy,:concept,:image).merge(user_id: current_user.id)
   end
 
-
   def set_prototype
     @prototype = Prototype.find(params[:id])
   end
+
   def move_to_index
     unless user_signed_in?
       redirect_to action: :index
