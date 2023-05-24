ff5fdac9076a16f046a970ba0dbc6d1dc6b98dab
taki39384 <132125615+taki39384@users.noreply.github.com>
2023-05-24 19:35:29 +0900

    コメント機能ビューファイルの修正

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "prototypes#index"
   resources :prototypes
-  resources :prototypes do
-    resources :comments, only: :create
+  resources :prototypes , only: [:new, :create] do
+    resources :comments, only: [:index, :create]
   end
 end
